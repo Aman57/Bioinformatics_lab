5a7209ca9918d4188a4f8237396bf91330feb780
Aman57 <59866128+Aman57@users.noreply.github.com>
2020-01-14 15:19:07 +0530

    Add files via upload

---
+++ amanlab3.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+$dna1='AGCTCCGCTA';
+print"1st DNA seq:",$dna1,"\n";
+$dna2='GCTCGACCA';
+print"2nd DNA seq:",$dna2,"\n";
+$dc1="$dna1$dna2";
+$dc2=$dna1.dna2;
+print"dna conc 1:",$dc1,"\n";
+print"dna conc 2:",$dc2,"\n";
+print"dna conc 3:",$dna1,$dna2,"\n";
+exit;
