6db3d3bd7c2193b453fa07b035bbcb4afad6cbd2
Aman57 <59866128+Aman57@users.noreply.github.com>
2020-01-21 16:15:46 +0530

    Add files via upload

---
+++ amanlab6.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+$word= 'OXYTOCIN';
+if($word eq'GLUTATHIONE')
+{
+print"GLUTATHIONE\n";
+}
+elsif($word eq'MELLITINE')
+{
+print"MELLITINE\n"
+}
+
+else
+{
+print"Is \"$word\" a peptide?This program is not sure. \n";
+}
+print"=======================================";
+print"\n\n";
+#exit;
